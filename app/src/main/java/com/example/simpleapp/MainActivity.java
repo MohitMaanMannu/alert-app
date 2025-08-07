ffe5e118fa5e26a8250b707b924ce29db2272b3f
otaval <45367703+otaval@users.noreply.github.com>
2025-08-07 16:22:15 +0530

    New

---
+++ app/src/main/java/com/example/simpleapp/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.simpleapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
